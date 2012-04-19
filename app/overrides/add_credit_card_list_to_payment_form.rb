6ac5da002d1846805395cb0e98847db68c0210c5
Dan Rasband <dan@agencyfusion.com>
2012-04-19 16:29:39 -0600

    Made compatible with spree 1.1.0.rc1.

--- app/overrides/add_credit_card_list_to_payment_form.rb
+++ app/overrides/add_credit_card_list_to_payment_form.rb
@@ -2,5 +2,7 @@ Deface::Override.new(
   :name => 'add_credit_card_list_to_payment_form',
   :virtual_path => 'spree/checkout/payment/_gateway',
   :insert_before => '[data-hook=card_number]',
-  :partial =>'spree/checkout/payment/existing_cards'
+  :partial =>'spree/checkout/payment/existing_cards',
+  :original => '558fa40b7734e939d98c3ba576aea1adc4f0ce00',
+  :disabled => false,
 )
